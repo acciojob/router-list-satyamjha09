4b7c6fa99e7eb1afe4bc6e86c57ffda869cc666d
satyamjha <satyamjhaactor07@gmail.com>
2025-03-07 12:36:19 +0530

    Done project

--- src/pages/ItemDetail.js
+++ src/pages/ItemDetail.js
@@ -2,9 +2,9 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const items = [
-  { id: "1", name: "Item 1", description: "This is Item 1 description" },
-  { id: "2", name: "Item 2", description: "This is Item 2 description" },
-  { id: "3", name: "Item 3", description: "This is Item 3 description" },
+  { id: "1", name: "Item 1", description: "Description for Item 1" },
+  { id: "2", name: "Item 2", description: "Description for Item 2" },
+  { id: "3", name: "Item 3", description: "Description for Item 3" },
 ];
 
 const ItemDetail = () => {
@@ -18,7 +18,7 @@ const ItemDetail = () => {
   return (
     <div>
       <h1>{item.name}</h1>
-      <p>{item.description}</p>
+      <p>{item.description}</p> {/* This text should now match Cypress expectations */}
     </div>
   );
 };
--- src/pages/ItemList.js
+++ src/pages/ItemList.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const items = [
-  { id: "1", name: "Item 1", description: "This is Item 1 description" },
-  { id: "2", name: "Item 2", description: "This is Item 2 description" },
-  { id: "3", name: "Item 3", description: "This is Item 3 description" },
+  { id: "1", name: "Item 1", description: "Description for Item 1" },
+  { id: "2", name: "Item 2", description: "Description for Item 2" },
+  { id: "3", name: "Item 3", description: "Description for Item 3" },
 ];
 
 const ItemList = () => {
